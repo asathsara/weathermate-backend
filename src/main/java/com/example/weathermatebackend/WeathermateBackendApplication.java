7a76c71c56bc5a57f400d33045b4b70e2bb9e4e0
A S <65203960+asathsara@users.noreply.github.com>
2025-08-25 18:33:13 +0530

    first commit

---
+++ src/main/java/com/example/weathermatebackend/WeathermateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.weathermatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathermateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeathermateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weathermatebackend/WeathermateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weathermatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathermateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
