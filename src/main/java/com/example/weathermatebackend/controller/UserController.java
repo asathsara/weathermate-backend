6b7ac18f9c54781644b4ffa3abd18234305ead93
A S <65203960+asathsara@users.noreply.github.com>
2025-08-25 19:51:10 +0530

    implement user controller

---
+++ src/main/java/com/example/weathermatebackend/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.weathermatebackend.controller;
+
+import com.example.weathermatebackend.model.Users;
+import com.example.weathermatebackend.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public Users register(@RequestBody Users user) {
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Users user) {
+        return userService.verify(user);
+    }
+}
