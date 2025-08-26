00c56b55a654591c3a34a372d61ea9c20fe2f3d5
A S <65203960+asathsara@users.noreply.github.com>
2025-08-26 18:37:09 +0530

    refactor: update User entity to specify table name

--- src/main/java/com/example/weathermatebackend/model/User.java
+++ src/main/java/com/example/weathermatebackend/model/User.java
@@ -1,12 +1,15 @@
 package com.example.weathermatebackend.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity
+@Entity(name = "users")
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
