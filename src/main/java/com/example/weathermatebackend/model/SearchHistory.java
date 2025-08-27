6bf943ee41af6764270ad289e61595e9b2e88659
A S <65203960+asathsara@users.noreply.github.com>
2025-08-27 15:45:27 +0530

    feat: add SearchHistory entity with user association

--- src/main/java/com/example/weathermatebackend/model/SearchHistory.java
+++ src/main/java/com/example/weathermatebackend/model/SearchHistory.java
@@ -1,4 +1,52 @@
 package com.example.weathermatebackend.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class SearchHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String city;
+    private LocalDateTime searchedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public LocalDateTime getSearchedAt() {
+        return searchedAt;
+    }
+
+    public void setSearchedAt(LocalDateTime searchedAt) {
+        this.searchedAt = searchedAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
