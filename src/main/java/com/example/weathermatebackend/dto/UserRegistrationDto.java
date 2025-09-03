358e34d550417888f27466d93d5bb05b08f2ca41
A S <65203960+asathsara@users.noreply.github.com>
2025-09-03 19:45:42 +0530

    add UserRegistrationDto

---
+++ src/main/java/com/example/weathermatebackend/dto/UserRegistrationDto.java
@@ -0,0 +1,18 @@
+package com.example.weathermatebackend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserRegistrationDto {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
+
+}
